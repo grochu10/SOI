2a7934b5c61682471e6692ca2087e14b2cc345fe
grochu10 <mati.groszek9@interia.pl>
2018-12-16 22:14:22 +0100

    Update queue.c

--- Projekt/src/queue.c
+++ Projekt/src/queue.c
@@ -17,9 +17,9 @@
 #include <uistd.h>
 
 #include <string.h>
-#include ".../inc/data.h"
-#include ".../inc/message.h"
-#include ".../inc/queue.h"
+#include "../inc/data.h"
+#include "../inc/message.h"
+#include "../inc/queue.h"
 
 
 
